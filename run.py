452401f7d7da887cf0e9228ab2f5c3c5ce507c6b
michelleconville <michelle.conville@gmail.com>
2023-01-16 21:15:57 +0000

    Add function to pick a random word from words.txt

--- run.py
+++ run.py
@@ -1,4 +1,15 @@
+import random
+
 import colorama
 from colorama import Fore, Style
 
 colorama.init(autoreset=True)
+
+
+
+def get_random_word():
+    """
+    Picks a random word from words.txt
+    """
+    random_word = random.choice(open("words.txt", "r").read().split('\n'))
+    return random_word.upper()
